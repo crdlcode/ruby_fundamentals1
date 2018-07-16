1ece0a6616572e444b5ae6eaadf08c0d4ce7cb73
curtischatch <curtis.chat.hk@gmail.com>
2018-07-16 16:07:43 -0400

    fini 4-3

---
+++ exercise4-3.rb
@@ -0,0 +1,10 @@
+
+my_name = "Curtis"
+puts "What's your first name?"
+user_input_name = gets.chomp
+
+if my_name == user_input_name
+  puts "We have the same name!"
+else
+  puts "cool.. #{user_input_name}"
+end
