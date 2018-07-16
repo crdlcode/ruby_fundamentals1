3f68af6dfcef95edaabb027a5cdea2752fe27e64
curtischatch <curtis.chat.hk@gmail.com>
2018-07-16 15:51:01 -0400

    fini 4-1

---
+++ exercise4-1.rb
@@ -0,0 +1,8 @@
+puts "Please enter an integer:"
+user_input_number = gets.to_i #the user enters a number
+
+if user_input_number > 100
+  puts "That's a big number"
+else
+  puts "why not dream a little bigger?"
+end
