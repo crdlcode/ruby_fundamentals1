ef2d64fe325f5a5e69870ac7a839d3ea060d97f6
curtischatch <curtis.chat.hk@gmail.com>
2018-07-16 15:11:07 -0400

    first commit

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+#1
+Bill = 55
+puts "Your total meal will cost $#{(Bill*1.15).round(2)}"
+#2
+puts "number:" + 2.to_s
+#3
+puts "There are #{45628*7839} humans that do"
+#4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
