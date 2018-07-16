ef2a51af5c1b1b77afdbef1da46582ff571fdc16
curtischatch <curtis.chat.hk@gmail.com>
2018-07-16 15:31:17 -0400

    extra done

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
-puts "what is your name?"
-user_name =gets
-puts "Hello, #{user_name}"
+# puts "what is your name?"
+# user_name =gets
+# puts "Hello, #{user_name}"
+
+puts "When were you born?"
+user_year =gets.chomp.to_i
+user_age =2018-user_year
+
+puts "You are #{user_age}"
